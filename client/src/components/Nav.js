750b2cbe9f9c1461814b251e720c045e188593b5
Fernando Ardente <ardente@mailbox.org>
2021-07-31 16:52:07 +0200

    Added skeleton navbar

---
+++ client/src/components/Nav.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+    return (
+        <div>
+            Nav
+            <Link to="/">Shopping List</Link>
+            <Link to="/recipes">Recipes</Link>
+        </div>
+    );
+}
