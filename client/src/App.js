a9a1325047dac6bb5616f3543df83be6ee2732bf
Fernando Ardente <ardente@mailbox.org>
2021-07-31 12:44:39 +0200

    Added Basic App Skeleton with Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,23 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import ShoppingList from "./components/ShoppingList";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+
 export default function App() {
-    return <div>Recipe Manager 0.0.1</div>;
+    return (
+        <div>
+            <h1>Recipe Manager 0.0.1</h1>
+            <BrowserRouter>
+                <section className="app">
+                    <header>
+                        <Nav />
+                    </header>
+                    <Route exact path="/" component={ShoppingList} />
+                    <Route path="/recipes" component={RecipeList} />
+                    <Route path="/recipe/:id" component={RecipeDetails} />
+                </section>
+            </BrowserRouter>
+        </div>
+    );
 }
