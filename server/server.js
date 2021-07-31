4e1bcd5c0cac99095c600c1ce1466f397bf04da7
Fernando Ardente <ardente@mailbox.org>
2021-07-31 12:45:55 +0200

    Added basic CRUD-Routes for shopping list and Middleware

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const path = require("path");
+const db = require("./db");
 
 const app = express();
 
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
+
+app.get("/api/shopping/items", async (req, res) => {
+    console.log("getting shopping items");
+    res.json(await db.getShoppingItems());
+});
+
+app.put("/api/shopping/check", async (req, res) => {
+    console.log("checking item", req.body);
+    res.json(await db.checkShoppingItem(req.body));
+});
+
+app.post("/api/shopping/delete", async (req, res) => {
+    console.log("Server deleting item", req.body);
+    res.json(await db.deleteShoppingItem(req.body));
+});
 
 app.get("*", function (req, res) {
     console.log("*ing");
     res.sendFile(path.join(__dirname, "..", "client", "public", "index.html"));
 });
+
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
