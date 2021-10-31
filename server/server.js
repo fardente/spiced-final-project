be6bb631f6ac3543b4121432f0baa45f0a5d991b
Fernando Ardente <ardente@mailbox.org>
2021-10-31 11:06:08 +0100

    Change file upload path

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const uploader = require("./uploader");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-const uploadLocation = "./uploads/";
+const uploadLocation = "/uploads/";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
